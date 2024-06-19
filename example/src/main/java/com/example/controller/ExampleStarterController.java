da6eaf8289d64f206a6c4ea4ebbf225e7dda0eaa
nikitaivanov <iwaa0303@mail.ru>
2024-06-19 21:11:04 +0300

    some fixes

--- example/src/main/java/com/example/controller/ExampleStarterController.java
+++ example/src/main/java/com/example/controller/ExampleStarterController.java
@@ -1,12 +1,12 @@
 package com.example.controller;
 
+import com.example.annotations.LogRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExampleStarterController {
-
 
     @LogRequest
     @GetMapping("/check/{arg}")
--- starter/src/main/java/com/example/aspect/LogRequestAspect.java
+++ starter/src/main/java/com/example/aspect/LogRequestAspect.java
@@ -1,11 +1,15 @@
 package com.example.aspect;
 
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.util.Enumeration;
 
